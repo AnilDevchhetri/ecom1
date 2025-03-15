59d584fc9ebdefdef46bdb8b644a5f93233d4c99
AnilDevchhetri <chhetrivision@gmail.com>
2025-03-15 22:18:53 +0900

    march 15

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,4 +33,4 @@ const App = () => {
 
 export default App
 
-//42
+//60
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -47,12 +47,17 @@ const Navbar = () => {
             </div>
 
             {/* Sidebar menu */}
-            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all duration-500 ease-in-out ${visible ? 'w-full' : 'w-0'}`}>
                 <div className="flex flex-col text-gray-600">
                     <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
                         <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="" />
                         <p>Back</p>
                     </div>
+                    <NavLink to='/' className="py-2 pl-6 border" onClick={() => setVisible(false)}>HOME</NavLink>
+                    <NavLink to='/collection' className="py-2 pl-6 border" onClick={() => setVisible(false)}>COLLECTION</NavLink>
+                    <NavLink to='/about' className="py-2 pl-6 border" onClick={() => setVisible(false)}>ABOUT</NavLink>
+                    <NavLink to='/contact' className="py-2 pl-6 border" onClick={() => setVisible(false)}>CONTACT</NavLink>
+
                 </div>
             </div>
 
