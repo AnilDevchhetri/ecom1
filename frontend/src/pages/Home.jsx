41dab9bad47a55bb14a1f35b6b907d84226b825a
AnilDevchhetri <chhetrivision@gmail.com>
2025-03-16 13:22:35 +0900

    march 16

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import { ShopContext } from '../context/ShopContext'
 
 const Home = () => {
-    const { products } = useContext(ShopContext);
+
     return (
         <>
             <Hero />
