1ebe8b69c7af634462a774d50d33d162c8ee7d6c
AnilDevchhetri <chhetrivision@gmail.com>
2025-04-13 13:44:02 +0900

    2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import BestSeller from '../components/BestSeller'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import NewsLetterBox from '../components/NewsLetterBox'
 import OurPolicy from '../components/OurPolicy'
 import { ShopContext } from '../context/ShopContext'
 
@@ -12,6 +13,7 @@ const Home = () => {
             <LatestCollection />
             <BestSeller />
             <OurPolicy />
+            <NewsLetterBox />
         </>
     )
 }
